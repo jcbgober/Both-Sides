b94413221985001c619fb2cbaa64e4cc6cc5375c
jcbgober <jcbgober@gmail.com>
2025-11-23 14:39:35 -0700

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-JavaScript
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // Add any future tweaks here, e.g., images: { domains: ['example.com'] }
-};
-
-export default nextConfig;
