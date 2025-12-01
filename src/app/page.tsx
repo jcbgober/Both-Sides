2ef005de15b7deb68360488a17ea4dd1d7331241
jcbgober <jcbgober@gmail.com>
2025-11-30 22:07:06 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = 'force-dynamic';
+
 import { useState } from "react";
 import topicsData from "@/data/topics.json";
 import TopicCard from "@/components/TopicCard";
